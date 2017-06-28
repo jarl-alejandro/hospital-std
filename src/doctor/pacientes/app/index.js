31f7f91bd13ded4cf8a62d3c8c05d045be9192f2
Alejandro Rivas <jarlalejor@gmail.com>
2017-06-28 16:04:49 -0500

    redirecionamientos los forms dependiendo del la edad

--- src/doctor/pacientes/app/index.js
+++ src/doctor/pacientes/app/index.js
@@ -2,9 +2,22 @@
 
 const doctor = angular.module('Hospital')
 
-doctor.controller('pacientesDoctorController', function ($scope, $http) {
+doctor.controller('pacientesDoctorController', function ($scope, $http, $location) {
   $scope.pacientes = []
 
   $http.get('src/doctor/pacientes/service/getAll.php')
     .then(response => $scope.pacientes = response.data)
+
+  $scope.form = function (object) {
+    const parametros = object.fecha.split('-')
+    const fecha = new Date(parametros[0], parametros[1] - 1, parametros[2])
+    const now = new Date()
+    const year = now.getFullYear() - fecha.getFullYear()
+    const moth = now.getMonth() - fecha.getMonth()
+    const age = (year * 12) + moth
+
+    if (age < 2) $location.path(`/form28A/${object.id}/${object.turno}`)
+    else $location.path(`/form28C/${object.id}/${object.turno}`)
+
+  }
 })
