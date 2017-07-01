0150fb527c2f0269ff3e0f2d7911a4434cf3d11c
Alejandro Rivas <jarlalejor@gmail.com>
2017-07-01 13:30:20 -0500

    integrando el menu a los roles

--- src/menu/app/index.js
+++ src/menu/app/index.js
@@ -2,7 +2,58 @@ angular.module('Hospital')
   .directive('hgcMenu', function ($rootScope) {
     return {
       restrict: 'E',
-      templateUrl: 'src/menu/menu.html',
+      templateUrl: 'src/menu/menuAdmin.html',
+      link: (scope, elem, attrs) => {
+
+        $(".button-collapse").sideNav({
+          closeOnClick: true,
+          draggable: true
+        })
+        $('.collapsible').collapsible()
+        $('.dropdown-button').dropdown()
+      }
+    }
+  })
+
+angular.module('Hospital')
+  .directive('hgcMenuDoctor', function ($rootScope) {
+    return {
+      restrict: 'E',
+      templateUrl: 'src/menu/doctor.html',
+      link: (scope, elem, attrs) => {
+
+        $(".button-collapse").sideNav({
+          closeOnClick: true,
+          draggable: true
+        })
+        $('.collapsible').collapsible()
+        $('.dropdown-button').dropdown()
+      }
+    }
+  })
+
+angular.module('Hospital')
+  .directive('hgcMenuEnfermera', function ($rootScope) {
+    return {
+      restrict: 'E',
+      templateUrl: 'src/menu/enfermera.html',
+      link: (scope, elem, attrs) => {
+
+        $(".button-collapse").sideNav({
+          closeOnClick: true,
+          draggable: true
+        })
+        $('.collapsible').collapsible()
+        $('.dropdown-button').dropdown()
+      }
+    }
+  })
+
+angular.module('Hospital')
+  .directive('hgcMenuEstadistica', function ($rootScope) {
+    return {
+      restrict: 'E',
+      templateUrl: 'src/menu/estadistica.html',
       link: (scope, elem, attrs) => {
 
         $(".button-collapse").sideNav({
