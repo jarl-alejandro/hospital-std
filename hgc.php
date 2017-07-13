45650ddd9a6bfc5a339cc18edad7928f71672307
Alejandro Rivas <jarlalejor@gmail.com>
2017-07-13 08:12:55 -0500

    respaldo

--- hgc.php
+++ hgc.php
@@ -5,6 +5,7 @@
     <title>Consultorio</title>
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/materialize.css">
+    <link rel="stylesheet" href="assets/css/select2.css">
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   </head>
