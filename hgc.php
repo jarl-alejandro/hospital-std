6abc21ec90eab0d7f906123fc2038c1588445347
Alejandro Rivas <jarlalejor@gmail.com>
2017-07-26 11:55:14 -0500

    agregando responsive desing al form 028A

--- hgc.php
+++ hgc.php
@@ -3,8 +3,8 @@
   <head>
     <meta charset="utf-8">
     <title>Consultorio</title>
-    <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/materialize.css">
+    <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/select2.css">
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
