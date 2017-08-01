7d4adfba2b965fdbcc5a8924a88dfc85293bb2f8
Alejandro Rivas <jarlalejor@gmail.com>
2017-08-01 10:02:53 -0500

    fixed bug of pacientes atendidos

--- src/enfermera/pacientes-atendidos/app/index.js
+++ src/enfermera/pacientes-atendidos/app/index.js
@@ -6,8 +6,5 @@ pacientesAtendidos.controller('pacAtenController', function ($scope, $http) {
   $scope.pacientes = []
 
   $http.get('src/enfermera/pacientes-atendidos/service/get.php')
-  .then(response => {
-    $scope.pacientes = response.data
-    console.log(response)
-  })
+  .then(response => $scope.pacientes = response.data)
 })
--- src/enfermera/pacientes-atendidos/service/get.php
+++ src/enfermera/pacientes-atendidos/service/get.php
@@ -3,8 +3,10 @@ include '../../../../helpers/conexion.php';
 date_default_timezone_set('America/Guayaquil');
 $hoy = date("Y-m-d");
 
-$query = $pdo->query("SELECT * FROM view_turnos WHERE hgc_esta_turno='signosVitales' 
-                          GROUP BY hgc_paci_turno");
+$query = $pdo->query("SELECT hgc_paci_turno, paciente, doctor, hgc_esta_turno,
+    MAX(hgc_id_turno) AS hgc_id_turno FROM view_turnos
+    GROUP BY hgc_paci_turno, paciente, doctor, hgc_esta_turno");
+
 $turnos = array();
 
 while ($row = $query->fetch()) {
