231712f20b5d9dfe82ca9b216fd8ffc452a0e5db
Alejandro Rivas <jarlalejor@gmail.com>
2017-08-07 08:29:28 -0500

    rename files and add image because the graphic SVG

--- src/doctor/form28C/app/grafica.js
+++ src/doctor/form28C/app/grafica.js
@@ -12,10 +12,10 @@ graphicForm28C.controller('graphicForm28Ctrl', function ($scope, $http) {
   function renderGraphic (sexo) {
 
     if (sexo === 'Hombre') {
-      $('.GraficaPeso img').attr('src', 'assets/img/graficas/niños-2meses_5años/peso.png')
-      $('.GraficaLongitud img').attr('src', 'assets/img/graficas/niños-2meses_5años/peso.png')
-      $('.GraficaCefalico img').attr('src', 'assets/img/graficas/niños-2meses_5años/peso.png')
-      $('.GraficaIMC img').attr('src', 'assets/img/graficas/niños-2meses_5años/peso.png')
+      $('.GraficaPeso img').attr('src', 'assets/img/graficas/ninos-2meses_5anos/peso.png')
+      $('.GraficaLongitud img').attr('src', 'assets/img/graficas/ninos-2meses_5anos/peso.png')
+      $('.GraficaCefalico img').attr('src', 'assets/img/graficas/ninos-2meses_5anos/peso.png')
+      $('.GraficaIMC img').attr('src', 'assets/img/graficas/ninos-2meses_5anos/peso.png')
 
       $('.GraficaPeso h4').html('PESO/EDAD - NIÑO MENOR DE 5 AÑOS (kg)')
       $('.GraficaLongitud h4').html('TALLA/EDAD - NIÑO MENOR DE 5 AÑOS (cm)')
@@ -25,10 +25,10 @@ graphicForm28C.controller('graphicForm28Ctrl', function ($scope, $http) {
       $('.footer-graphic').html('CURVA DE CRECIMIENTO DEL NIÑO MENOR DE 5 AÑOS')
       $('.footer-msp').html('MPS HCU - Form 028 A2/09')
     } else {
-      $('.GraficaPeso img').attr('src', 'assets/img/graficas/niñas-2mese_5años/peso.png')
-      $('.GraficaLongitud img').attr('src', 'assets/img/graficas/niñas-2mese_5años/peso.png')
-      $('.GraficaCefalico img').attr('src', 'assets/img/graficas/niñas-2mese_5años/peso.png')
-      $('.GraficaIMC img').attr('src', 'assets/img/graficas/niñas-2mese_5años/peso.png')
+      $('.GraficaPeso img').attr('src', 'assets/img/graficas/ninas-2mese_5anos/peso.png')
+      $('.GraficaLongitud img').attr('src', 'assets/img/graficas/ninas-2mese_5anos/peso.png')
+      $('.GraficaCefalico img').attr('src', 'assets/img/graficas/ninas-2mese_5anos/peso.png')
+      $('.GraficaIMC img').attr('src', 'assets/img/graficas/ninas-2mese_5anos/peso.png')
 
       $('.GraficaPeso h4').html('PESO/EDAD - NIÑA MENOR DE 5 AÑOS (kg)')
       $('.GraficaLongitud h4').html('TALLA/EDAD - NIÑA MENOR DE 5 AÑOS (cm)')
