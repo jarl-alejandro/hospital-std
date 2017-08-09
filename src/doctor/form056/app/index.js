34fadbb8eed57b764f415bbd9a92804bb920a32e
Alejandro Rivas <jarlalejor@gmail.com>
2017-08-09 17:01:45 -0500

    form 056

--- src/doctor/form056/app/index.js
+++ src/doctor/form056/app/index.js
@@ -2,4 +2,20 @@
 
 angular.module('Hospital')
 .controller('formCtrl056', function ($scope, $http, $stateParams) {
+  $scope.paciente = {}
+  $scope.empresa = {}
+
+  $http.get(`src/doctor/form056/service/paciente.php?id=${$stateParams.id}`)
+  .then(response => {
+    $scope.paciente = response.data.paciente
+    $scope.apellidos = $scope.paciente.hgc_ape_pacie.split(" ")
+    $scope.provincia = `${$scope.paciente.hgc_desc_provi}, ${$scope.paciente.hgc_desc_canton}, ${$scope.paciente.hgc_desc_parro}`
+    console.log($scope.paciente)
+  })
+
+  $http.get('src/config/empresa/service/get.php')
+  .then(response => {
+    if (response.data.cont === 1) $scope.empresa = response.data.empresa
+  })
+
 })
