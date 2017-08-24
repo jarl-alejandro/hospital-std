5c072b62bafbf4e2aac98a934fe31cb8f595a8cc
Alejandro Rivas <jarlalejor@gmail.com>
2017-08-24 10:33:23 -0500

    hoja de devolucion editada

--- src/doctor/hoja-devolucion/service/edit.php
+++ src/doctor/hoja-devolucion/service/edit.php
@@ -7,8 +7,7 @@ include '../../../../helpers/generar_codigo.php';
 $json = file_get_contents('php://input');
 $obj = json_decode($json);
 
-$codigo = setCode($pdo, 'F056-', 9, 'hgc_form056', 'hgc_cont_form056');
-updateCode($pdo, 'hgc_cont_form056');
+$codigo = $obj->codigoForm;
 
 $paciente = $obj->paciente;
 $ncon_qs = $pdo->query("SELECT hgc_ncon_f056 FROM hgc_form056 WHERE hgc_paci_f056='$paciente'");
@@ -31,40 +30,28 @@ $form = $pdo->prepare("INSERT INTO hgc_form056 (hgc_codi_f056, hgc_paci_f056, hg
   hgc_estu_f056,hgc_nomen_f056, hgc_ulmen_f056)
   VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 
-$form->bindParam(1, $codigo);
-$form->bindParam(2, $paciente);
-$form->bindParam(3, $numeroConsulta);
-$form->bindParam(4, $hoy);
-$form->bindParam(5, $companion);
-$form->bindParam(6, $civilStatus);
-$form->bindParam(7, $enfermedadActual);
-$form->bindParam(8, $turno);
-$form->bindParam(9, $planTratamiento);
-$form->bindParam(10, $fechaProxima);
-$form->bindParam(11, $tanner);
-$form->bindParam(12, $estudios);
-$form->bindParam(13, $noMenstruacion);
-$form->bindParam(14, $menstruacion);
+$form = $pdo->prepare("UPDATE hgc_form056 SET hgc_paci_f056=?, hgc_ncon_f056=?, hgc_fech_f056=?, hgc_acom_f056=?,
+  hgc_civi_f056=?, hgc_enfe_f056=?, hgc_turno_f056=?, hgc_indic_f056=?, hgc_fetpr_f056=?, hgc_tanne_f056=?,
+  hgc_estu_f056=?, hgc_nomen_f056=?, hgc_ulmen_f056=? WHERE hgc_codi_f056=?");
+
+$form->bindParam(1, $paciente);
+$form->bindParam(2, $numeroConsulta);
+$form->bindParam(3, $hoy);
+$form->bindParam(4, $companion);
+$form->bindParam(5, $civilStatus);
+$form->bindParam(6, $enfermedadActual);
+$form->bindParam(7, $turno);
+$form->bindParam(8, $planTratamiento);
+$form->bindParam(9, $fechaProxima);
+$form->bindParam(10, $tanner);
+$form->bindParam(11, $estudios);
+$form->bindParam(12, $noMenstruacion);
+$form->bindParam(13, $menstruacion);
+$form->bindParam(14, $codigo);
 $form->execute();
 
-$estado = 'solicitud';
-$tipoTurno = 'subsecuente';
-$doctorTurno = $obj->doctorTurno;
-
-if ($fechaProxima != '') {
-  $new = $pdo->prepare("INSERT INTO hgc_turno (hgc_paci_turno, hgc_doct_turno, hgc_esta_turno,
-    hgc_fech_turno, hgc_tipo_turno) VALUES (?, ?, ?, ?, ?)");
-
-    $new->bindParam(1, $paciente);
-    $new->bindParam(2, $doctorTurno);
-    $new->bindParam(3, $estado);
-    $new->bindParam(4, $fechaProxima);
-    $new->bindParam(5, $tipoTurno);
-    $new->execute();
-}
-
+$pdo->query("DELETE FROM hgc_motivocie10_f056 WHERE hgc_form_f056='$codigo'");
 $motivo = $obj->motivo;
-
 $motivo_qs = $pdo->prepare("INSERT INTO hgc_motivocie10_f056 (hgc_form_f056, hgc_cie_f056, hgc_tipo_f056) VALUES (?,?,?)");
 
 foreach ($motivo as $row) {
