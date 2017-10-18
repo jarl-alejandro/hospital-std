838730b6fe306e396930720a2acf7fc451d1ca43
Alejandro Rivas <jarlalejor@gmail.com>
2017-10-18 13:23:24 -0500

    solicitud terminada

--- src/doctor/solicitud/app/index.js
+++ src/doctor/solicitud/app/index.js
@@ -3,10 +3,99 @@
 angular.module('Hospital')
 .controller('SolicitudCtrl', function ($scope, $http) {
   $scope.empresa = {}
-  $('.browser-default').select2()
+  $scope.form = { paciente: '', especialidad: '', medico: '', tipo: '' }
+  $scope.pacientes = []
+  $scope.espcialidades = []
+  $scope.medicos = []
 
-  $http.get('src/doctor/receta/service/empresa.php')
-  .then(response => $scope.empresa = response.data)
+  $scope.isLoader = false
+  $scope.enableSend = false
+
+  let $select = $('.browser-default')
+  $select.select2()
+  getData()
+
+  $scope.save = () => {
+    if (valid()) {
+      $scope.form.fecha = $('#inicio').val()
+        $scope.enableSend = true
+
+      $http.post('src/doctor/solicitud/service/save.php', $scope.form)
+      .then(response => {
+        console.log(response)
+        if (response.data === '201') {
+          Materialize.toast('Se ha guardado con exito', 4000)
+          $scope.form = { paciente: '', especialidad: '', medico: '', tipo: '' }
+          $('#paciente').val('')
+          $('#especialidad').val('')
+          $('#medico').val('')
+          $('#tipo').val('')
+          $('#inicio').val('')
+          $select.trigger('change.select2')
+        }
+        else {
+          Materialize.toast('Tenemos problemasa', 4000)
+          console.log(response);
+        }
+        $scope.enableSend = false
+
+      })
+    }
+  }
+
+  $scope.changeEspecialidad = () => {
+    if ($scope.form.especialidad !== null) {
+      $http.get(`src/doctor/receta/service/medico.php?especialidad=${$scope.form.especialidad}`)
+      .then(response => {
+        $scope.medicos = response.data
+      })
+    }
+  }
+
+  function valid () {
+    let form = $scope.form
+
+    if (!form.tipo) {
+      Materialize.toast('Ingrese el tipo', 4000)
+      $('#tipo').focus()
+      return false
+    }
+    if (!form.paciente) {
+      Materialize.toast('Ingrese el paciente', 4000)
+      $('#paciente').focus()
+      return false
+    }
+    if (!form.especialidad) {
+      Materialize.toast('Ingrese la especialidad', 4000)
+      $('#especialidad').focus()
+      return false
+    }
+    if (!form.medico) {
+      Materialize.toast('Ingrese el medico', 4000)
+      $('#medico').focus()
+      return false
+    }
+    if (!$('#inicio').val().trim()) {
+      Materialize.toast('Ingrese la fecha', 4000)
+      $('#inicio').focus()
+      return false
+    }
+    else return true
+  }
+
+  function getData () {
+    $http.get('src/doctor/receta/service/empresa.php')
+    .then(response => $scope.empresa = response.data)
+
+    $http.get('src/doctor/receta/service/pacientes.php')
+    .then(response => {
+      $scope.pacientes = response.data
+      $scope.isLoader = true
+    })
+
+    $http.get('src/doctor/receta/service/especialidad.php')
+    .then(response => $scope.espcialidades = response.data)
+  }
 
   $('.datepicker').pickadate({
     selectMonths: true,
@@ -24,6 +113,6 @@ angular.module('Hospital')
     labelMonthPrev: 'Mes Anterior',
     labelMonthSelect: 'Selecione el mes',
     labelYearSelect: 'Selecione el año',
-    max: new Date()
+    min: new Date()
   });
 })
---
+++ src/doctor/solicitud/service/save.php
@@ -0,0 +1,30 @@
+<?php
+include '../../../../helpers/conexion.php';
+include '../../../../helpers/generar_codigo.php';
+date_default_timezone_set('America/Guayaquil');
+
+$json = file_get_contents('php://input');
+$obj = json_decode($json);
+
+$codigo = setCode($pdo, 'SL-', 9, 'hgc_solicitud', 'hgc_cont_solicitud');
+updateCode($pdo, 'hgc_cont_solicitud');
+
+$paciente = $obj->paciente;
+$especialidad = $obj->especialidad;
+$medico = $obj->medico;
+$tipo = $obj->tipo;
+
+$qs = $pdo->prepare("INSERT INTO hgc_solicitud (hgc_cod_soli, hgc_pac_soli, hgc_esps_soli, hgc_med_soli,
+  hgc_tip_soli) VALUES (?, ?, ?, ?, ?)");
+
+$qs->bindParam(1, $codigo);
+$qs->bindParam(2, $paciente);
+$qs->bindParam(3, $especialidad);
+$qs->bindParam(4, $medico);
+$qs->bindParam(5, $tipo);
+
+$qs->execute();
+
+if ($qs) {
+  echo 201;
+}
